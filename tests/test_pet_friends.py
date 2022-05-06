e78e8c469d28e11e5fed926854e5ab99f67e7041
evamalis <evamalis@mail.ru>
2022-05-06 21:16:33 +0300

    petfriends

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -6,14 +6,14 @@ import os
 pf = PetFriends()
 
 
-def text_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
 
     assert status == 200
     assert 'key' in result
 
 
-def get_all_pets_with_valid_key(filter=''):
+def test_get_all_pets_with_valid_key(filter=''):
     _, auth_key = pf.get_api_key(valid_email, valid_password)
     status, result = pf.get_list_of_pets(auth_key, filter)
 
